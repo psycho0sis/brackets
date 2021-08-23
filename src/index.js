02f5a91ffb08a7717a62ca4c0d4f44acf9d360ff
psycho0sis <ilonakot92@gmail.com>
2021-08-23 18:08:56 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const OPEN_BRACKETS = [
+    "[",
+    "(",
+    "|",
+    "{",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+  ];
+  const BRACKETS_MAP = {
+    ["]"]: "[",
+    [")"]: "(",
+    ["|"]: "|",
+    ["}"]: "{",
+    ["1"]: "1",
+    ["2"]: "2",
+    ["2"]: "1",
+    ["3"]: "3",
+    ["4"]: "4",
+    ["4"]: "3",
+    ["5"]: "5",
+    ["6"]: "6",
+    ["6"]: "5",
+    ["7"]: "7",
+    ["8"]: "8",
+  };
+
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+
+    if (OPEN_BRACKETS.includes(currentSymbol)) {
+      stack.push(currentSymbol);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      let topElement = stack[stack.length - 1];
+
+      if (BRACKETS_MAP[currentSymbol] === topElement) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
